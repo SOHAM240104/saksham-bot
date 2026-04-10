422df651c8090a5f4bbfb4da1a88c7d3cf78cd3f
SOHAM240104 <114135339+SOHAM240104@users.noreply.github.com>
2026-04-10 18:02:09 +0530

    chore: stop tracking migrations/ (local-only SQL; keep out of remote)

--- migrations/002_drop_legacy_scam_collection.sql
+++
@@ -1,9 +0,0 @@
--- Remove legacy PGVector collection named ``scam`` after migrating scam content to
--- ``scam_kb`` (or whatever SCAM_VECTOR_COLLECTION is set to).
-
-DELETE FROM langchain_pg_embedding
-WHERE collection_id IN (
-    SELECT uuid FROM langchain_pg_collection WHERE name = 'scam'
-);
-
-DELETE FROM langchain_pg_collection WHERE name = 'scam';
