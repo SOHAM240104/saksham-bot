c9462360f9f8a4905b0f39eb9a318980aab420b9
SOHAM240104 <114135339+SOHAM240104@users.noreply.github.com>
2026-04-29 12:51:30 +0530

    added_chatmodels

--- app/models/config.py
+++ app/models/config.py
@@ -5,3 +5,10 @@ USER_TYPE_ENUM = Enum("user", "senior", "tech_saathi", name="user_type")
 GENDER_ENUM = Enum("male", "female", "other", name="gender")
 ONBOARDING_TYPE_ENUM = Enum("self", "assisted", name="onboarding_type")
 
+THREAD_ROLE_ENUM = Enum("chatbot", "techsaathi", name="thread_role")
+
+THREAD_STATUS_ENUM = Enum(
+    "assigned",
+    "resolved",
+    name="thread_status"
+)
